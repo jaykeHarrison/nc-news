494733ca5a91390d2d3565f0301cf7dd514a1f10
jaykeHarrison <jayke@jaykeharrison.co.uk>
2022-06-17 12:49:14 +0100

    add deleteComment api util func

--- src/utils/api.js
+++ src/utils/api.js
@@ -69,3 +69,11 @@ export const addCommentToArticleByID = (article_id, username, body) => {
       return response.data.addedComment;
     });
 };
+
+export const deleteComment = (comment_id) => {
+  return ncNewsAPI.delete(`/comments/${comment_id}sdjfsdjf`).catch((err) => {
+    if (err) {
+      throw new Error("delete failed");
+    }
+  });
+};
