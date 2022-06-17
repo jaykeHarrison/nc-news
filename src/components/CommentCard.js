adef98dbf4b206429a8714616f59071c82a1e66d
jaykeHarrison <jayke@jaykeharrison.co.uk>
2022-06-17 12:49:59 +0100

    add DeleteButton to comments

--- src/components/CommentCard.js
+++ src/components/CommentCard.js
@@ -1,18 +1,27 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { UserContext } from "../contexts/UserContext";
+import DeleteButton from "./DeleteButton";
 
 const CommentCard = ({ comment }) => {
+  const [isDeleted, setIsDeleted] = useState(false);
   const { signedInUser } = useContext(UserContext);
 
   return (
     <li
       className={`comment-card ${
         signedInUser === comment.author ? "signed-in-user" : null
-      }`}
+      } ${isDeleted ? "comment-card-deleted" : null}`}
     >
       <p>{comment.author}</p>
       <p>{comment.body}</p>
       <p>{comment.created_at}</p>
+      {signedInUser === comment.author ? (
+        <DeleteButton
+          comment_id={comment.comment_id}
+          isDeleted={isDeleted}
+          setIsDeleted={setIsDeleted}
+        />
+      ) : null}
     </li>
   );
 };
